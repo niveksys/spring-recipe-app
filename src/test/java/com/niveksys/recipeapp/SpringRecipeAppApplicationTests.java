28b5ebe6405e3fd82c67f73f3257802499f808cd
Kevin Tsang <niveksys@gmail.com>
2021-01-15 16:42:07 +0800

    Created initial spring boot project

---
+++ src/test/java/com/niveksys/recipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niveksys.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
