c77d663ad4a78eb93e7fd0aae8bdcbdac6d13755
Kevin Tsang <niveksys@gmail.com>
2021-01-17 00:31:26 +0800

    Added JPA Enumerations on Difficulty.

---
+++ src/main/java/com/niveksys/recipeapp/model/Difficulty.java
@@ -0,0 +1,5 @@
+package com.niveksys.recipeapp.model;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/niveksys/recipeapp/model/Recipe.java
+++ src/main/java/com/niveksys/recipeapp/model/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -32,6 +34,9 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -122,6 +127,14 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
 }
