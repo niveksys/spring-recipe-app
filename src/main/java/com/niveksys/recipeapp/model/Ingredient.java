804f5d0fbbf87efc6db4b6cab6a4db15bd6bdc23
Kevin Tsang <niveksys@gmail.com>
2021-01-17 00:05:40 +0800

    Added One to One Relationship among Ingredient and UnitOfMeasure.

--- src/main/java/com/niveksys/recipeapp/model/Ingredient.java
+++ src/main/java/com/niveksys/recipeapp/model/Ingredient.java
@@ -3,10 +3,12 @@ package com.niveksys.recipeapp.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -15,7 +17,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    // private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
