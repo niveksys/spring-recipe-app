df88a98993aab5d44c976646216b0a6845643707
Kevin Tsang <niveksys@gmail.com>
2021-02-01 18:17:42 +0800

    Added recipeform.html for Create Recipe.

--- src/main/java/com/niveksys/recipeapp/controller/RecipeController.java
+++ src/main/java/com/niveksys/recipeapp/controller/RecipeController.java
@@ -1,13 +1,17 @@
 package com.niveksys.recipeapp.controller;
 
+import com.niveksys.recipeapp.command.RecipeCommand;
 import com.niveksys.recipeapp.service.RecipeService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/recipe")
 public class RecipeController {
     private final RecipeService recipeService;
 
@@ -15,10 +19,22 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/show/{id}")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", this.recipeService.findById(Long.parseLong(id)));
-
         return "recipe/show";
     }
+
+    @RequestMapping("/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = this.recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
 }
